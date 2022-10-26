fbce8ed8ab2aba555f3ae79aeb11488b7c3e4d7b
sunsmile <320663557@qq.com>
2022-10-26 10:07:08 +0800

    fix window is not defind

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,7 +4,10 @@ import locale from 'element-ui/lib/locale/lang/zh-CN'; // lang i18n
 
 import CommHeader from '@src/components/CommHeader.vue';
 
-export default ({ Vue }) => {
+export default (config) => {
+  const { Vue, isServer } = config;
+  if (isServer) return;
+
   Vue.component('comm-header', CommHeader); // 全局注册外来组件 自动注册的全局组件需要放在 .vuepress/components 目录下
   Vue.use(ElementUI, { locale });
 
